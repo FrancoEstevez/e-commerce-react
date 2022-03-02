45e3da2dc11ddce4b3dcb9aa98330afdd08f7e35
Franco <francoestevez20008@gmail.com>
2022-03-01 22:02:02 -0300

    counter hecho

--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Tittle from '../Tittle'
+import React, { useState } from "react";
+import Tittle from "../Tittle";
+import Count from "../Count";
+
+const onAdd = (cant) => {
+  console.log(cant);
+};
 
 const ItemListContainer = () => {
-    return (
-        <div>
-            <Tittle tittle= {'Tittle'} subtittle='Subtittle' greeting={() => console.log('Saludos')}/>
-        </div>
-    )
-}
+  return (
+    <div>
+    <Tittle tittle= {'Tittle'} subtittle='Subtittle' greeting={() => console.log('Saludos')}/>
+      <Count initial={1} stock={9} onAdd={onAdd} />
+    </div>
+  );
+};
 
 export default ItemListContainer;
