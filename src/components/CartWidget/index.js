12b4e1352715cdad7be32012bad9cc0733fa68ec
Franco <francoestevez20008@gmail.com>
2022-03-22 23:13:12 -0300

    pagina con intento de card

--- src/components/CartWidget/index.js
+++ src/components/CartWidget/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import './CartWidget.css'
+// import React from 'react'
+// import './CartWidget.css'
 
-const CartWidget = () =>{
-    return(
-        <p>Cart</p>
+// const CartWidget = () =>{
+//     return(
+//         <p>Cart</p>
+//     )
+// }
+
+// export default CartWidget;
+
+import { useCartContext } from "../CardContext/cartContext"
+
+
+function Cart() {
+    const { cartList, vaciarCart } = useCartContext()
+    console.log(cartList)
+    return (
+      <div>
+        cart
+        { cartList.map(productos => <li>nombre: {productos.brand} precio: {productos.price} cantidad:{productos.cantidad}</li>) }
+        <button onClick={vaciarCart}>VaciarCarrto</button>
+      </div>
     )
 }
 
-export default CartWidget;
+export default Cart
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,14 +1,27 @@
 import { useParams } from 'react-router-dom';
+import { useState } from "react"
 import React from 'react';
 import './style.css'
 import Intercambiabilidad from '../Intercambiabilidad';
 import Count from '../Count';
-
+import { Link } from "react-router-dom"
+import { useCartContext } from "../CardContext/cartContext"
 
 const ItemDetail = ({producto}) => {
+  const [count, setCount] = useState(null)
   const { detailId } = useParams()
   console.log(detailId)
-  const onAdd = (item) => console.log(item);
+ //  const onAdd = (item) => console.log(item);
+
+  const { agregarCart, cartList } = useCartContext()
+
+  const onAdd = cant =>{
+    console.log(cant)
+    setCount(cant)
+    agregarCart({ ...producto, cantidad: cant })
+  }
+  console.log(cartList)
+
   return (
     <div>
       <div className='detailcontainer'>
@@ -26,6 +39,10 @@ const ItemDetail = ({producto}) => {
       </div>
         <Count initial={1} stock={10} onAdd={onAdd} />
         <Intercambiabilidad />
+
+        <Link to='/cart'>
+              <button className='btn btn-outline-primary'>Ir al Cart</button>
+        </Link> 
     </div>
   )
 }
