0d0fc96e9f455eaf3679b959b29cb8c29cb06030
Franco <francoestevez20008@gmail.com>
2022-03-29 20:28:16 -0300

    cart terminado

--- src/components/CartWidget/index.js
+++ src/components/CartWidget/index.js
@@ -1,29 +1,57 @@
 import { useCartContext } from "../CartContext/cartContext"
 import './cartWidget.css'
+import { Link } from "react-router-dom";
 
 function Cart() {
   const { cartList, emptyCart, substractItem, totalPrice } = useCartContext()
   console.log(cartList)
   return (
-    <div>
-      <div className="leftContainer">
-        <div>
-          {cartList.map(product => (
-            <div className="cartItemContainer" key={product.id}>
-                <img src={product.image} alt="" className='imageCart' />
-              <div className="infoItemCart">
-                <p className="cartCarName">{`${product.brand} ${product.model}`}</p>
-                <p>U$D{product.price}</p>
-                <p>amount: {product.cantidad}</p>
+
+    <div className="container container-fluid">
+      <div>
+        {cartList.length > 0 ?
+          <div>
+            <div className="leftContainer">
+              <div>
+                {cartList.map(product => (
+                  <div className="cartItemContainer" key={product.id}>
+                    <img src={product.image} alt="" className='imageCart' />
+                    <div className="infoItemCart">
+                      <p className="cartCarName">{`${product.brand} ${product.model}`}</p>
+                      <p>U$D{product.price}</p>
+                      <p>amount: {product.cantidad}</p>
+                    </div>
+                    <button className="deleteCartItems" onClick={() => { substractItem(product) }}>{product.cantidad === 1 ? "x" : "-"}</button>
+                  </div>
+                ))}
               </div>
-              <button className="deleteCartItems" onClick={() => { substractItem(product) }}>{product.cantidad === 1 ? "x" : "-"}</button>
+              <div className="rightContainer">
+                <div className="rightContainer">
+                  Total: U$D{totalPrice()}
+                </div>
+              </div>
             </div>
-          ))}
-        </div>
-        <button className="emptyCart" onClick={emptyCart}>Empty cart</button>
-      </div>
-      <div className="rightContainer">
-            Total: U$D{totalPrice()}
+            <div className="emptyCartBtn">
+              <button className="btn btn-outline-secondary" onClick={emptyCart}>
+                Empty Cart
+              </button>
+            </div>
+          </div>
+          :
+          <div className="cartBanner">
+            <div className="cartBannerHeader">
+              <p className="cartBannerText">Your cart is empty. </p>
+              <p className="cartBannerText">
+                Start shopping through the varity of books in our page.{" "}
+              </p>
+            </div>
+            <Link to="/">
+              <button className="cartBtn btn btn-outline-success">
+                Shop Now
+              </button>
+            </Link>
+          </div>
+        }
       </div>
     </div>
   )
